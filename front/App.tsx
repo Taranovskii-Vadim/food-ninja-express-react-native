6e51515b4d54257825daced5ac98003b12d50550
vgTaranovsky <taranovscki.vadim@yandex.ru>
2022-05-13 14:55:40 +0400

    feat(master): add new splash image

--- front/App.tsx
+++ front/App.tsx
@@ -1,4 +1,8 @@
+import React, { useState, useEffect, useCallback } from "react";
 import { StyleSheet, Text, View } from "react-native";
+import * as SplashScreen from "expo-splash-screen";
+
+// TODO do we have to import React in react native apps???
 
 const styles = StyleSheet.create({
   container: {
@@ -8,10 +12,51 @@ const styles = StyleSheet.create({
   },
 });
 
-const App = (): JSX.Element => (
-  <View style={styles.container}>
-    <Text>Hello react-native</Text>
-  </View>
-);
+const App = (): JSX.Element | null => {
+  // TODO can create one common hook
+  const [isReady, setIsReady] = useState(() => false);
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        // Keep the splash screen visible while we fetch resources
+        await SplashScreen.preventAutoHideAsync();
+        // Pre-load fonts, make any API calls you need to do here
+        // await Font.loadAsync(Entypo.font);
+        // Artificially delay for two seconds to simulate a slow loading
+        // experience. Please remove this if you copy and paste the code!
+        await new Promise((resolve) => setTimeout(resolve, 2000));
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        // Tell the application to render
+        setIsReady(true);
+      }
+    }
+
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (isReady) {
+      // This tells the splash screen to hide immediately! If we call this after
+      // `setAppIsReady`, then we may see a blank screen while the app is
+      // loading its initial state and rendering its first pixels. So instead,
+      // we hide the splash screen once we know the root view has already
+      // performed layout.
+      await SplashScreen.hideAsync();
+    }
+  }, [isReady]);
+
+  if (!isReady) {
+    return null;
+  }
+
+  return (
+    <View style={styles.container} onLayout={onLayoutRootView}>
+      <Text>Hello react-native</Text>
+    </View>
+  );
+};
 
 export default App;
---
+++ front/src/pages/Cart/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { View } from "react-native";
+
+const Cart = () => {
+  return <View>Cart</View>;
+};
+
+export default Cart;
