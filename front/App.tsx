f48fdd746823e1ee917ca425719695de4dbd05fd
vgTaranovsky <taranovscki.vadim@yandex.ru>
2022-05-13 16:25:31 +0400

    feat(master): code refactor

--- front/App.tsx
+++ front/App.tsx
@@ -19,17 +19,12 @@ const App = (): JSX.Element | null => {
   useEffect(() => {
     async function prepare() {
       try {
-        // Keep the splash screen visible while we fetch resources
         await SplashScreen.preventAutoHideAsync();
-        // Pre-load fonts, make any API calls you need to do here
         // await Font.loadAsync(Entypo.font);
-        // Artificially delay for two seconds to simulate a slow loading
-        // experience. Please remove this if you copy and paste the code!
         await new Promise((resolve) => setTimeout(resolve, 2000));
       } catch (e) {
         console.warn(e);
       } finally {
-        // Tell the application to render
         setIsReady(true);
       }
     }
@@ -39,11 +34,6 @@ const App = (): JSX.Element | null => {
 
   const onLayoutRootView = useCallback(async () => {
     if (isReady) {
-      // This tells the splash screen to hide immediately! If we call this after
-      // `setAppIsReady`, then we may see a blank screen while the app is
-      // loading its initial state and rendering its first pixels. So instead,
-      // we hide the splash screen once we know the root view has already
-      // performed layout.
       await SplashScreen.hideAsync();
     }
   }, [isReady]);
