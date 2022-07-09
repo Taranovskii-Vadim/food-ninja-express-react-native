35b3732c0af80c92607da50865bf3d5020433f3f
vgTaranovsky <taranovscki.vadim@yandex.ru>
2022-07-09 20:25:01 +0400

    reinit expo

--- front/App.tsx
+++ front/App.tsx
@@ -1,52 +1,20 @@
-import React, { useState, useEffect, useCallback } from "react";
+import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
-import * as SplashScreen from "expo-splash-screen";
-
-// TODO do we have to import React in react native apps???
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
   },
 });
 
-const App = (): JSX.Element | null => {
-  // TODO can create one common hook
-  const [isReady, setIsReady] = useState(() => false);
-
-  useEffect(() => {
-    async function prepare() {
-      try {
-        await SplashScreen.preventAutoHideAsync();
-        // await Font.loadAsync(Entypo.font);
-        await new Promise((resolve) => setTimeout(resolve, 2000));
-      } catch (e) {
-        console.warn(e);
-      } finally {
-        setIsReady(true);
-      }
-    }
-
-    prepare();
-  }, []);
-
-  const onLayoutRootView = useCallback(async () => {
-    if (isReady) {
-      await SplashScreen.hideAsync();
-    }
-  }, [isReady]);
-
-  if (!isReady) {
-    return null;
-  }
-
-  return (
-    <View style={styles.container} onLayout={onLayoutRootView}>
-      <Text>Hello react-native</Text>
-    </View>
-  );
-};
+const App = (): JSX.Element => (
+  <View style={styles.container}>
+    <Text>Open up App.js to start working on your app!</Text>
+    <StatusBar style="auto" />
+  </View>
+);
 
 export default App;
--- front/src/pages/Cart/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { View } from "react-native";
-
-const Cart = () => {
-  return <View>Cart</View>;
-};
-
-export default Cart;
